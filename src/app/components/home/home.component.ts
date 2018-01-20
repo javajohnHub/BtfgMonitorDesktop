2475571720fc20e6c8fb0b01da37828f7bae3a22
javajohnhub <joedw1978@gmail.com>
2018-01-20 13:11:35 -0500

    small changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,59 +13,116 @@ export class HomeComponent implements OnInit {
   walletData;
   blockData;
   shareList;
+  shareList2;
   shareArray = [];
   loading;
+  pendingPayments;
+  pendingPayment;
+  sentPayments;
+  lastPayment;
+  lastBlock;
+  balance;
+  blockReward;
+  sentPayment;
+  shareList3;
+  totalShares;
   constructor(private _btfgService: BtfgService) { }
 
   ngOnInit() {
-    if(localStorage.getItem('walletId')){
+    if (localStorage.getItem('walletId')) {
       this.walletId = localStorage.getItem('walletId');
+      this.getData();
     }
     this.loading = true;
-    this.paymentData = this._btfgService.getPaymentInfo()
-    .subscribe(data => {
-      this.paymentData = data;
-      this.shareList = this.paymentData.blockPaymentList;
-      this.loading = false;
-    });
 
-    
+    this.paymentData = this._btfgService.getPaymentInfo()
+      .subscribe(data => {
+        this.paymentData = data;
+        this.pendingPayments = this.paymentData.pendingPaymentList;
+        this.pendingPayment = this.pendingPayments[this.walletId];
+        this.sentPayments = this.paymentData.sentPaymentList;
+        this.sentPayment = this.sentPayments[this.walletId];
+        this.shareList = this.paymentData.blockPaymentList[0].shareList;
+        this.paymentData.blockPaymentList.forEach(blockPayment => {
+          this.totalShares = parseFloat(this.totalShares) + parseFloat(blockPayment.totalShare);
+          console.log(this.totalShares.toFixed(8) )
+        })
+        this.shareList2 = this.paymentData.blockPaymentList[1].shareList;
+        this.shareList3 = this.paymentData.blockPaymentList[2].shareList;
+        if (this.shareList) {
+          this.shareList.forEach(share => {
+            //console.log(share['accountId'], this.walletId)
+            if (share['accountId'] == this.walletId) {
+              //console.log(share)
+              this.shareArray.push(share);
+            }
+          })
+        }
+        if (this.shareList2) {
+          this.shareList2.forEach(share => {
+            if (share['accountId'] == this.walletId) {
+              this.shareArray.push(share);
+            }
+
+          })
+        }
+        if (this.shareList3) {
+          this.shareList3.forEach(share => {
+            if (share['accountId'] == this.walletId) {
+              this.shareArray.push(share);
+            }
+
+          })
+        }
+        if (this.sentPayments) {
+          this.sentPayments.forEach(payment => {
+            if (payment.accountId == this.walletId) {
+              this.lastPayment = payment;
+            }
+          })
+
+        }
+        this.loading = false;
+      });
+
     this.loading = true;
     this.blockChainStatus = this._btfgService.getBlockchainStatus()
-    .subscribe(data => {
-      this.blockChainStatus = data;
-      this.loading = false;
-    });
+      .subscribe(data => {
+        this.blockChainStatus = data;
+        this.lastBlock = this.blockChainStatus.numberOfBlocks;
+        this.loading = false;
+      });
 
     this.loading = true;
     this.blockData = this._btfgService.getBlockInfo()
-    .subscribe(data => {
-      this.blockData = data;
-      this.loading = false;
-    });
+      .subscribe(data => {
+        this.blockData = data;
+        this.blockReward = this.blockData.blocks[0].blockReward;
+        this.loading = false;
+      });
   }
 
   getData() {
     this.loading = true;
-    if(!localStorage.getItem('walletId')){
+    if (!localStorage.getItem('walletId')) {
       localStorage.setItem('walletId', this.walletId)
     }
-    
+
     this.walletData = this._btfgService.getWalletInfo(this.walletId)
-    .subscribe(data => {
-      this.walletData = data;
-      if(this.shareList){
-        if(this.shareList[this.walletId]){
-          console.log(this.shareList[this.walletId])
-          this.shareArray.push(this.shareList[this.walletId])
-        }
-      }
-      this.loading = false;
-    });
-    
-  }
-  get getShareArray(){
-    return this.shareArray;
+      .subscribe(data => {
+        this.walletData = data;
+        this.balance = this.walletData.effectiveBalanceNXT;
+        this.shareList.forEach(share => {
+
+          if (share.accountId == parseInt(this.walletId, 10)) {
+
+            this.shareArray.push(share.share)
+          }
+        })
+
+      })
+    this.loading = false;
+
   }
 
 }
