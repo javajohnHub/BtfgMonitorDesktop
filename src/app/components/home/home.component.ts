6313c6802fef3a2a689f3cb110c8bb98210c59ab
javajohnhub <joedw1978@gmail.com>
2018-01-23 19:46:16 -0500

    its working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -51,70 +51,70 @@ export class HomeComponent implements OnInit {
   _burstData() {
     this.loading = true;
     this._btfgService.getBlockchainStatus()
-    .subscribe((data) => {
-      this.loading = true;
-      this.currentBlock = data['numberOfBlocks'];
-      this.loading = false;
-    }, (err) => console.log(err))
+      .subscribe((data) => {
+        this.loading = true;
+        this.currentBlock = data['numberOfBlocks'];
+        this.loading = false;
+      }, (err) => console.log(err))
     this._btfgService.getBlockInfo()
-    .subscribe((data) => {
-      this.loading = true;
-      this.blockData = data;
-    }, (err) => console.log(err), () => {
-      this.blockReward = this.blockData['blocks'][0]['blockReward']
-      this.loading = false;
-    })
+      .subscribe((data) => {
+        this.loading = true;
+        this.blockData = data;
+      }, (err) => console.log(err), () => {
+        this.blockReward = this.blockData['blocks'][0]['blockReward']
+        this.loading = false;
+      })
     this._btfgService.getPaymentInfo()
-    .subscribe((data) => {
-      this.loading = true;
-      this.paymentData = data;
-      if(this.paymentData){
-        if (this.paymentData['pendingPaymentList'][this.walletId]) {
-          this.pendingPayment = this.paymentData['pendingPaymentList'][this.walletId]
-        }else {
-          this.pendingPayment = 0;
-        }
-        for (const pending_id of this.paymentData['pendingPaymentList']) {
-          this.totalPending += this.paymentData['pendingPaymentList'][pending_id]
-        }
-        for (const block of this.paymentData['blockPaymentList']) {
-          this.blockLabels.push(block['height']);
-          this.totalShare += block['totalShare']
-          let shareFound = 0;
-          for (const share of block['shareList']) {
-            if ( share['accountId'] === this.walletId) {
-              this.blockShares.push(share['share']);
-              this.minerShare += share['share'];
-              shareFound = 1;
+      .subscribe((data) => {
+        this.loading = true;
+        this.paymentData = data;
+        if (this.paymentData) {
+          if (this.paymentData['pendingPaymentList'][this.walletId]) {
+            this.pendingPayment = this.paymentData['pendingPaymentList'][this.walletId]
+          } else {
+            this.pendingPayment = 0;
+          }
+          for (const pending_id of this.paymentData['pendingPaymentList']) {
+            this.totalPending += this.paymentData['pendingPaymentList'][pending_id]
+          }
+          for (const block of this.paymentData['blockPaymentList']) {
+            this.blockLabels.push(block['height']);
+            this.totalShare += block['totalShare']
+            let shareFound = 0;
+            for (const share of block['shareList']) {
+              if (share['accountId'] === this.walletId) {
+                this.blockShares.push(share['share']);
+                this.minerShare += share['share'];
+                shareFound = 1;
+              }
+            }
+            if (shareFound === 0) {
+              this.blockShares.push(0.0);
             }
           }
-          if (shareFound === 0) {
-            this.blockShares.push(0.0);
-          }
         }
-      }
-      if (this.totalShare === 0) {
-        this.estimatedRevenue = 0;
-      }else {
-        this.estimatedRevenue =  (this.minerShare * this.blockReward) / this.totalShare
-      }
-      this.loading = false;
+        if (this.totalShare === 0) {
+          this.estimatedRevenue = 0;
+        } else {
+          this.estimatedRevenue = (this.minerShare * this.blockReward) / this.totalShare
+        }
+        this.loading = false;
       })
     this._btfgService.getWalletInfo(this.walletId)
-    .subscribe((data) => {
-      this.loading = true;
-      this.walletData = data;
-      this.currentBalance = (this.walletData['effectiveBalanceNXT'] / 100000000);
-      this.address = this.walletData['accountRS'];
-      if ( 'name' in this.walletData) {
-        this.name = this.walletData['name']
-      }
-      if ('description' in this.walletData) {
-        this.description = this.walletData['description']
-      }
-    }, (err) => console.log(err), () => {
-      this.loading = false;
-    })
+      .subscribe((data) => {
+        this.loading = true;
+        this.walletData = data;
+        this.currentBalance = (this.walletData['effectiveBalanceNXT'] / 100000000);
+        this.address = this.walletData['accountRS'];
+        if ('name' in this.walletData) {
+          this.name = this.walletData['name']
+        }
+        if ('description' in this.walletData) {
+          this.description = this.walletData['description']
+        }
+      }, (err) => console.log(err), () => {
+        this.loading = false;
+      })
   }
 
 }
